6a8b2b7a3f8808a1795a65130da6e20908539a3d
Eelco <eelco.bosklopper@capgemini.com>
2021-06-02 11:33:34 +0200

    Gave hello some colour

--- pages/hello.js
+++ pages/hello.js
@@ -3,7 +3,18 @@ import { useState, useEffect } from "react";
 export default function Hello() {
   return (
     <>
-      <h1>Hello from another page!</h1>
+      <span
+        style={{
+          position: "absolute",
+          top: "0",
+          left: "0",
+          background: "lightsalmon",
+          height: "100%",
+          width: "100%",
+          border: 'solid 5px'
+        }}
+      ></span>
+      <h1 style={{ position: "absolute", width: '100%', textAlign: 'center' }}>Hello from another page!</h1>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -41,6 +41,6 @@ export async function getServerSideProps(context) {
   );
 
   return {
-    props: {}, // will be passed to the page component as props
+    props: {},
   };
 }
